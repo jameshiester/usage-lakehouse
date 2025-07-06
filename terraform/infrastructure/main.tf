61d45c6e931f1c5ead6d16d8b8aea6842ae14836
James Hiester <james.hiester@fake.com>
2025-07-06 17:50:07 -0400

    updating vpc config

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -52,25 +52,25 @@ module "vpc" {
   tags = local.tags
 }
 
-resource "aws_security_group" "lambda" {
-  name        = format("%s%s%s%s", var.Prefix, "scg", var.EnvCode, "lambda")
-  description = " Application Security Group"
-  vpc_id      = module.vpc.vpc_id
+# resource "aws_security_group" "lambda" {
+#   name        = format("%s%s%s%s", var.Prefix, "scg", var.EnvCode, "lambda")
+#   description = " Application Security Group"
+#   vpc_id      = module.vpc.vpc_id
 
-  egress {
-    description = "Application Outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     description = "https"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name         = format("%s%s%s-%s", var.Prefix, "scg", var.EnvCode, "lambda")
-    resourcetype = "security"
-    codeblock    = "network-3tier"
-  }
-}
+#   tags = {
+#     Name         = format("%s%s%s-%s", var.Prefix, "scg", var.EnvCode, "lambda")
+#     resourcetype = "security"
+#     codeblock    = "network-3tier"
+#   }
+# }
 
 resource "aws_vpc_security_group_egress_rule" "postgres" {
   security_group_id = aws_security_group.lambda.id
