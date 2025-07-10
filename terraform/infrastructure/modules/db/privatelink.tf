a9e46219e2ac06ab5e16f6076b6b60f6c185aea7
James Hiester <james.hiester@fake.com>
2025-07-10 18:28:47 -0400

    updating principals for service endpoint

--- terraform/infrastructure/modules/db/privatelink.tf
+++ terraform/infrastructure/modules/db/privatelink.tf
@@ -187,6 +187,7 @@ resource "aws_vpc_endpoint_service" "rds_lb_endpoint_service" {
   network_load_balancer_arns = [aws_lb.rds_lb.arn]
 
   supported_regions = [var.Region]
+  allowed_principals = ["arn:aws:iam::${ data.aws_caller_identity.current.account_id}:root"]
 
   tags = {
     Name = format("%s-%s-%s", var.Prefix, "rds-endpoint", var.EnvCode)
