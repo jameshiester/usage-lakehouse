06a581ee0ce923a7c04c0a826b9f73647f5d5eff
James Hiester <james.hiester@fake.com>
2025-07-10 00:02:27 -0400

    fixing terraform config

--- terraform/infrastructure/modules/db/privatelink.tf
+++ terraform/infrastructure/modules/db/privatelink.tf
@@ -129,7 +129,7 @@ EOF
 }
 
 resource "aws_sns_topic_policy" "default" {
-  arn = aws_sns_topic.test.arn
+  arn = aws_sns_topic.default.arn
 
   policy = data.aws_iam_policy_document.sns_topic_policy.json
 }
